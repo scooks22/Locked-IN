1efe70cc971eab27f302b053a3057544d917d36a
Kevin Marin <Qbz16@txstate.edu>
2026-03-23 23:45:51 -0500

    feat: implement Player and Utils classes AI: [Prompt] asked for Player class with color attribute and Utils class with position parsing and validation helpers; [Adopt] used getColor() method for Player and parsePosition() and isValidPosition() methods for Utils; [Change] kept Player simple for Phase 1

--- ChessGame/utils/Position.java
+++ ChessGame/utils/Position.java
@@ -1,2 +1,34 @@
 package ChessGame.utils;
 
+public class Position {
+    
+    private int row;
+    private int column;
+    
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+    public void setRow(int row) {
+        this.row = row;
+    }
+    
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
+    public String toString() {
+        char file = (char) ('A' + column);
+        int rank = row + 1;
+        return "" + file + rank;
+    }
+}
